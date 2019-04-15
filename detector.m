e435df9488ff300177be78465d3a66d82623ce5c
Oliver Zhang <oliverzhang1@yahoo.com>
2019-04-15 16:26:44 -0400

    Complete detection pipeline

--- detector.m
+++ detector.m
@@ -3,14 +3,15 @@ clear all
 patch_size = 20;
 num_lines = 5;
 line_dist_thresh = 3;
+line_prune_thresh = 1;
 
 %% Image preprocessing
 im = imread('plank.png');
-%im = imread('plank_blur.jpg');
+% im = imread('plank_blur.jpg');
 %im = imgaussfilt(im, 1);
 
-% im = imread('IMG_3761_small.JPG');
-% im = imresize(im, 0.3);
+% im = imread('IMG_3761_cropped.jpg');
+%im = imresize(im, 0.9);
 % im = getMasked(im);
 
 gray = rgb2gray(im);
@@ -20,36 +21,43 @@ C = corner(gray);
 %% Find edge candiates from patches
 [im_padded, patches, edges, lines_patch, lines_world] = getPatchLines(gray,C,patch_size, num_lines);
 eqs = getLineEquations(lines_world);
-plotCorners(1, im_padded, C, patch_size);
-% plotLines(1, im_padded, eqs)
+% plotCorners(2, im_padded, C + patch_size, patch_size, 'c');
+% plotLines(1, im_padded, eqs, 'r:')
 % plotPatches(1, C, patches, edges, lines_patch, num_lines)
 
 %% Find matched lines
 eqs_dist = matchLines(eqs, C + patch_size, im_padded, line_dist_thresh);
-[eqs_matched, matches] = refineMatches(eqs_dist, C);
+% plotLines(1, im_padded, eqs_dist(:, 6:7), 'r:')
+plotCorners(1, im_padded, C+patch_size, 0, 'r');
+[C_pruned] = pruneVertices(eqs_dist, C + patch_size, line_prune_thresh, im_padded);
+plotCorners(1, [], C_pruned(:, 1:2), 2, 'c');
 
-plotMatchedLines(1, [], eqs_matched);
+[eqs_matched, matches] = refineMatches(eqs_dist, C_pruned);
+
+plotLines(1, [], eqs_matched(:,6:7), 'g--')
+plotMatchedLines(2, im_padded, eqs_matched);
 
 %% Find correct vertices
-[vertices, eqs] = getValidVertices();
+[vertices, eqs_valid] = getValidVertices(matches, eqs_matched);
+plotValidVertices(3, im_padded, vertices, eqs_valid);
 
 %% Plotting
-function plotCorners(fig, im_padded, C, patch_size)
+function plotCorners(fig, im_padded, C, patch_size, color)
     figure(fig)
     if(size(im_padded,1) > 0)
         imshow(im_padded);
     end
     hold on
     for i = 1:size(C,1)
-        x = C(i,1) + patch_size;
-        y = C(i,2) + patch_size;
-        plot(x,y, 'c*')
-        text(x+patch_size-5,y-patch_size-5,"  " + i, 'Color','cyan')
-        rectangle('Position',[x-patch_size, y-patch_size, 2*patch_size+1, 2*patch_size+1], 'EdgeColor', 'c')
+        x = C(i,1);
+        y = C(i,2);
+        plot(x,y, '*', 'Color', color)
+        text(x+patch_size-5,y-patch_size-5,"  " + i, 'Color',color)
+        rectangle('Position',[x-patch_size, y-patch_size, 2*patch_size+1, 2*patch_size+1], 'EdgeColor', color)
     end
     hold off
 end
-function plotLines(fig, im_padded, eqs)
+function plotLines(fig, im_padded, eqs, style)
     figure(fig)
     if(size(im_padded,1) > 0)
         imshow(im_padded);
@@ -61,7 +69,7 @@ function plotLines(fig, im_padded, eqs)
         for k = 1:size(eqs, 1)
             eq = eqs(k, :, i);
             [x, y] = getEqPlotPoints(eq, width);
-            plot(x,y,'LineWidth',.25,'Color','r', 'LineStyle', ':');
+            plot(x,y,style,'LineWidth',.25);
         end
     end
     hold off
@@ -99,8 +107,27 @@ function plotMatchedLines(fig, im_padded, eqs_matched)
         plot(x,y,'g--', 'LineWidth',.5);
         c0 = eqs_matched(k, 8:9);
         c1 = eqs_matched(k, 10:11);
-        plot([c0(1) c1(1)], [c0(2) c1(2)], 'y', 'LineWidth', 1.5);
+        plot([c0(1) c1(1)], [c0(2) c1(2)], 'y*-', 'LineWidth', 1.5);
     end
+    hold off
+end
+function plotValidVertices(fig, im_padded, vertices, eqs_valid)
+    figure(fig)
+    if(size(im_padded,1) > 0)
+        imshow(im_padded);
+    end
+    pos = get(gcf, 'Position'); %// gives x left, y bottom, width, height
+    width = pos(3);
+    hold on
+    for i = 1:size(eqs_valid, 1)
+        eq = eqs_valid(i, 6:7);
+        [x, y] = getEqPlotPoints(eq, width);
+        plot(x,y,'g--', 'LineWidth',.5);
+        c0 = eqs_valid(i, 8:9);
+        c1 = eqs_valid(i, 10:11);
+        plot([c0(1) c1(1)], [c0(2) c1(2)], 'y*-', 'LineWidth', 1.5);
+    end
+    plot(vertices(:,1), vertices(:,2), 'co', 'MarkerFaceColor', 'c')
     hold off
 end
 function [x, y] = getEqPlotPoints(eq, width)
@@ -115,36 +142,121 @@ function [x, y] = getEqPlotPoints(eq, width)
 end
 
 %% Functions
-function [vertices, valid_eqs] = getValidVertices()
-    valid = find(sum(matches, 2) >= 4);
+function [vertices, eqs_valid] = getValidVertices(matches, eqs_matched)
+    valid = find(sum(matches, 2) == 4);
+    vertices = [];
+    eqs_valid = zeros(0, 11);
     for i = 1:size(valid)
         v = valid(i);
         mask = (eqs_matched(:, 1) == v) | (eqs_matched(:, 2) == v);
-
         eqs = eqs_matched(mask,6:7);
         [xi, yi] = getBestIntersection(eqs);
+        vertices = [vertices; [xi, yi]];
+        eqs_valid = [eqs_valid; eqs_matched(mask, :)];
     end
 end
-function [eqs_matched, matches] = refineMatches(eqs, C)
-    matches = eye(size(C, 1));
-    eqs_matched = zeros(0, size(eqs, 2));
-    for i = 1:size(C, 1)
-        for j = i+1:size(C,1)
-            rows = (eqs(:,1) == i) & (eqs(:,2) == j);
-            eqs_i = eqs(rows, :);
-            for k = 1:size(eqs_i, 1)
-                dist0 = eqs_i(k,4);
-                dist1 = eqs_i(k,3);
+function [eqs_matched, matches] = refineMatches(eqs_dist, C_pruned)
+    matches = eye(size(C_pruned, 1));
+    eqs_matched = zeros(0, size(eqs_dist, 2));
+    % Loop over each possible vertex pairing
+    for i = 1:size(C_pruned, 1) 
+        for j = i+1:size(C_pruned,1)
+            % Get eqs that are connecting the vertices
+            rows = (eqs_dist(:,1) == C_pruned(i, 3) & (eqs_dist(:,2) == C_pruned(j, 3)));
+            eqs_i = eqs_dist(rows, :);
+            if(size(eqs_i, 1) > 0)
                 matches(i, j) = matches(i,j) + 1;
                 matches(j, i) = matches(j,i) + 1;
-                eqs_matched = [eqs_matched; eqs_i(k,:)];
-                break
+                for m = 1:size(eqs_i)
+                    eq_avg = eqs_i(m,:);
+                    if(sum(~isfinite(eq_avg)) == 0)
+                        break
+                    end
+                end
+                if(sum(~isfinite(eq_avg)) ~= 0)
+                    break
+                end
+%                 eq_avg = sum(eqs_i, 1)/size(eqs_i, 1);
+                eq_avg(1) = C_pruned(i,3);
+                eq_avg(2) = C_pruned(j,3);
+                eqs_matched = [eqs_matched; eq_avg];
             end
         end
     end
+    eqs_matched = sortrows(eqs_matched);
 end
-function [eqs_matched] = matchLines(eqs, C, im_padded, thresh)
-    eqs_matched = zeros(0, 11);
+function [C_pruned] = pruneVertices(eqs, C, line_prune_thresh, im_padded)
+    invalid = false(size(C, 1), 1);
+    % Loop over each possible vertex pairing
+    for i = 1:size(C, 1) 
+        if(invalid(i))
+            continue
+        end
+        for j = i+1:size(C,1)
+            if(invalid(j))
+                continue;
+            end
+            % Get eqs that are connecting the vertices
+            rows = (eqs(:,1) == i) & (eqs(:,2) == j);
+            eqs_i = eqs(rows, :);
+            if(size(eqs_i, 1) > 0)
+                %eq_avg = sum(eqs_i, 1)/size(eqs_i, 1);
+                for m = 1:size(eqs_i)
+                    eq_avg = eqs_i(m,:);
+                    if(sum(~isfinite(eq_avg)) == 0)
+                        break
+                    end
+                end
+                if(sum(~isfinite(eq_avg)) ~= 0)
+                    break
+                end
+                % Loop over every vertex that aren't the connected ones
+                valid_corners = [];
+                invalid_corners = [];
+                for v = 1:size(C, 1)
+                    if(v ~= i && v ~= j && ~invalid(v))
+                        m = eq_avg(6);
+                        b = eq_avg(7);
+                        x = C(v,1);
+                        y = C(v,2);
+                        dist = getDistToLine(m,b, x, y);    
+                        % Check if vertex is on the line
+                        if(dist < line_prune_thresh)
+                            [xp, yp] = projectPoint(m, b, x, y);
+                            x0 = eq_avg(8);
+                            y0 = eq_avg(9);
+                            x1 = eq_avg(10);
+                            y1 = eq_avg(11);
+                            dist0 = sqrt((x0-xp)^2 + (y0-yp)^2);
+                            dist1 = sqrt((x1-xp)^2 + (y1-yp)^2);
+                            distp = sqrt((x1-x0)^2 + (y1-y0)^2);
+                            
+                            % Check if vertex is between others
+                            if abs(((dist0 + dist1)-distp)) < 0.01
+                                invalid(v) = 1;
+                                invalid_corners = [invalid_corners;C(v,:)];
+                            else
+                                valid_corners = [valid_corners;C(v,:)];
+                            end
+                        else
+                            valid_corners = [valid_corners;C(v,:)];
+                        end
+                    end
+                end
+%                 C_avg = [eq_avg(8:9);eq_avg(10:11)];
+%                 plotCorners(2, im_padded, C_avg, 0, 'c');
+%                 plotLines(2, [], eq_avg(:,6:7), 'g')
+%                 plotLines(2, [], eqs_i(:,6:7), 'b:')
+%                 plotCorners(2, [], invalid_corners, 20, 'r');
+%                 plotCorners(2, [], valid_corners, 0, 'g');
+%                 pause(0.1);
+            end
+        end
+    end
+    C_pruned = [C(~invalid, :), find(~invalid)];
+end
+function [eqs_dist] = matchLines(eqs, C, im_padded, thresh)
+    eqs_dist = zeros(0, 11);
     for i=1:size(eqs,3)
         for k=1:size(eqs,1)
             eq = eqs(k, :, i);
@@ -159,15 +271,16 @@ function [eqs_matched] = matchLines(eqs, C, im_padded, thresh)
                 if(dist0 > thresh || dist1> thresh)
                     continue
                 end
+
                 if(i < j)
-                    eqs_matched = [eqs_matched; [i, j, dist1, dist0, length, eq, p0, p1]];
+                    eqs_dist = [eqs_dist; [i, j, dist1, dist0, length, eq, p0, p1]];
                 else
-                    eqs_matched = [eqs_matched; [j, i, dist1, dist0, length, eq, p1, p0]];
+                    eqs_dist = [eqs_dist; [j, i, dist1, dist0, length, eq, p1, p0]];
                 end
             end
         end
     end
-    eqs_matched = sortrows(eqs_matched);
+    eqs_dist = sortrows(eqs_dist);
 end
 function [im_padded, patches, edges, lines_patch, lines_world] = getPatchLines(im,C,patch_size, num_lines)
     im_padded = padarray(im, [patch_size patch_size], 0, 'both');
